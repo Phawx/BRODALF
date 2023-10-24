f029dbc424b5b6644e05103fa4241fe9a6f6324e
Phawx <carygolomb@gmail.com>
2023-10-23 22:35:11 -0400

    Implemented basic GUI with Build/Index and Load Index functionalities. Added MD5 hashing for files.

--- main.go
+++ main.go
@@ -5,12 +5,25 @@ import (
 	"os"
 	"path/filepath"
 
+	"crypto/md5"
+	"encoding/hex"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
+
+func calculateMD5(filePath string) (string, error) {
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	hasher := md5.New()
+	hasher.Write(fileData)
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
 
 // ListFiles returns a list of directories and files for a given path.
 func ListFiles(dir string) ([]string, error) {
@@ -28,6 +41,38 @@ func ListFiles(dir string) ([]string, error) {
 	return files, err
 }
 
+type FileData struct {
+	Path    string
+	Hash    string
+	Backups int // To track the number of backups for this file
+}
+
+func indexDirectory(dirPath string) ([]FileData, error) {
+	var filesData []FileData
+
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			hash, err := calculateMD5(path)
+			if err != nil {
+				return err
+			}
+			filesData = append(filesData, FileData{
+				Path:    path,
+				Hash:    hash,
+				Backups: 0, // We'll update this later when implementing backup functionality
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return filesData, nil
+}
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("BRODALF - Backup Explorer")

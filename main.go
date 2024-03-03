20487f4a3ad6f0db801e0b33c9857699c5142837
Phawx <carygolomb@gmail.com>
2024-03-03 17:48:20 -0500

    Added MD5 toggle

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-
+	"compress/gzip"
 	"crypto/md5"
 	"encoding/hex"
-
-	"compress/gzip"
 	"encoding/json"
+	"os"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -41,35 +39,18 @@ func calculateMD5(filePath string) (string, error) {
 	return hex.EncodeToString(hasher.Sum(nil)), nil
 }
 
-// ListFiles returns a list of directories and files for a given path.
-func ListFiles(dir string) ([]string, error) {
-	var files []string
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		// To only add items from the root directory (and not subdirectories)
-		if filepath.Dir(path) == dir {
-			files = append(files, info.Name())
-		}
-		return nil
-	})
-	return files, err
-}
-
 type FileData struct {
 	Path    string
 	Hash    string
-	Backups int // To track the number of backups for this file
+	Backups int
 }
 
-func indexDirectory(dirPath string, progressBar *widget.ProgressBar) ([]FileData, error) {
+func indexDirectory(dirPath string, progressBar *widget.ProgressBar, hashingCheckbox *widget.Check) ([]FileData, error) {
 	var filesData []FileData
 
-	totalFiles, _ := filepath.Glob(dirPath + "/*") // Just an approximation
+	totalFiles, _ := filepath.Glob(dirPath + "/*")
 	progressStep := 1.0 / float64(len(totalFiles))
 
-	// Show and reset the progress bar
 	progressBar.Show()
 	progressBar.SetValue(0)
 
@@ -78,9 +59,14 @@ func indexDirectory(dirPath string, progressBar *widget.ProgressBar) ([]FileData
 			return err
 		}
 		if !info.IsDir() {
-			hash, err := calculateMD5(path)
-			if err != nil {
-				return err
+			var hash string
+			if hashingCheckbox.Checked {
+				hash, err = calculateMD5(path)
+				if err != nil {
+					return err
+				}
+			} else {
+				hash = "Not Calculated"
 			}
 			filesData = append(filesData, FileData{
 				Path:    path,
@@ -92,18 +78,20 @@ func indexDirectory(dirPath string, progressBar *widget.ProgressBar) ([]FileData
 		return nil
 	})
 
-	progressBar.Hide() // Hide the progress bar when done
+	progressBar.Hide()
 	return filesData, err
 }
+
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("BRODALF - Backup Explorer")
 	myWindow.Resize(fyne.NewSize(800, 600))
 
 	progressBar := widget.NewProgressBar()
-	progressBar.Hide() // Initially hidden, will be shown during indexing
+	progressBar.Hide()
 
-	// Button to build/index folder/file structure
+	hashingCheckbox := widget.NewCheck("Enable MD5 Hashing", nil)
+
 	buildIndexBtn := widget.NewButton("Build/Index Folder/File Structure", func() {
 		dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
 			if err != nil {
@@ -111,39 +99,25 @@ func main() {
 				return
 			}
 			if uri == nil {
-				// User canceled or closed the dialog without selecting a directory
 				return
 			}
-
-			// Index the selected directory
-			filesData, err := indexDirectory(uri.Path(), progressBar)
+			filesData, err := indexDirectory(uri.Path(), progressBar, hashingCheckbox)
 			if err != nil {
 				dialog.ShowError(err, myWindow)
 				return
 			}
-
-			// For now, save the index data to a file named "index.gz" in the current directory
-			// This can be changed later based on user input or configuration
 			err = saveIndexToFile(filesData, "index.gz")
 			if err != nil {
 				dialog.ShowError(err, myWindow)
-				return
 			}
 		}, myWindow)
 	})
 
-	// Button to load the index file
 	loadIndexBtn := widget.NewButton("Load the Index File", func() {
-		// TODO: Implement the functionality to load the index file
+		// TODO: Implement loading functionality
 	})
 
-	// Layout the buttons vertically
-	content := container.NewVBox(
-		buildIndexBtn,
-		loadIndexBtn,
-		progressBar,
-	)
-
+	content := container.NewVBox(buildIndexBtn, loadIndexBtn, hashingCheckbox, progressBar)
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 }
